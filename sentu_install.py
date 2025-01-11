0bf47ad16d0d4a05710dd61f5f4c8af56741ea28
Luis Guillermo Echenique <elepistemedev@gmail.com>
2025-01-11 19:32:06 -0400

    FIX: arreglada la dirección del archivo main.py

--- sentu_install.py
+++ sentu_install.py
@@ -31,6 +31,7 @@ def download_and_extract(repo_url, temp_dir):
 def execute_phase1(temp_dir):
     """Ejecuta el script de la primera fase desde el directorio temporal."""
     main_script = temp_dir / "dotfiles-dev" / "src" / "phase1" / "main.py"
+    print(f"archivo main.py esta en: {str(main_script)}")
     if main_script.exists():
         print("Ejecutando Fase 1 desde el directorio temporal...")
         try:
